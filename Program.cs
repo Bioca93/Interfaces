3f19b7a59464dfd1de857ce37a830e0d6d8925e4
Beatriz Ressurreição <bea_jrr_93@hotmail.com>
2023-07-28 12:23:24 +0100

    first deploy

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassInterfaces;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int gasoline = 0;
+       
+        Car car = new Car();
+        
+        car.Refuel(gasoline);
+        car.Drive(gasoline);
+        
+       
+    }
+}
